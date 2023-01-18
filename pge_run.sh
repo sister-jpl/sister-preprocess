6c4cfd2c28320f47968f6865a462b75019c032cf
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-01-18 15:33:37 -0800

    Debugging code added

--- pge_run.sh
+++ pge_run.sh
@@ -2,6 +2,12 @@
 
 source activate sister
 
+ls -l
+
+pwd
+
+echo "Creating runconfig"
 python generate_runconfig.py inputs.json
 
+echo "Running L1 Preprocess PGE"
 python l1_preprocess.py runconfig.json
