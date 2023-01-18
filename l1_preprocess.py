a03472db988ad254cbd4d6e6f71884c30d92935e
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-01-18 12:03:47 -0800

    Updated file names

--- l1_preprocess.py
+++ l1_preprocess.py
@@ -1,89 +1,180 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+'''
 SISTER
 Space-based Imaging Spectroscopy and Thermal PathfindER
 Author: Adam Chlus
 
-"""
+'''
 
-import argparse
 import glob
 import os
-from sister.sensors import prisma,aviris,desis
+import shutil
+import sys
+import tarfile
+import json
+import hytools as ht
 from hytools.io import parse_envi_header
-
+import numpy as np
+from PIL import Image
+from sister.sensors import prisma,aviris,desis
+from sister.utils import download_file
 
 def main():
-    parser = argparse.ArgumentParser(description = "Convert PRISMA HDF to ENVI format")
-    parser.add_argument('l1_file',help="Path to compressed input file", type = str)
-    parser.add_argument('out_dir',help="Output directory", type = str)
-    parser.add_argument('temp_dir',help="Temporary directory", type = str)
-    parser.add_argument('resolution',help="Output resample resolution",type=int, default = 30)
-    parser.add_argument('smile', nargs='?',help="Path to smile wavelengths", default = False)
-    parser.add_argument('rad_coeff', nargs='?',help="Path to radiometric coeffs",default = False)
-    parser.add_argument('landsat', nargs='?',help="Landsat reference file",default = False)
 
-    args = parser.parse_args()
-    base_name = os.path.basename(args.l1_file)
+    run_config_json = sys.argv[1]
+
+    with open(run_config_json, 'r') as in_file:
+        run_config =json.load(in_file)
+
+    base_name = os.path.basename(run_config['inputs']['l1_granule'])
+
+    os.mkdir('output')
+    os.mkdir('temp')
 
     aws_cop_url='https://copernicus-dem-30m.s3.amazonaws.com/'
 
     if base_name.startswith('PRS'):
-        prisma.he5_to_envi(args.l1_file,args.out_dir,args.temp_dir,
+
+        smile = 'data/prisma/PRISMA_Mali1_wavelength_shift_surface_smooth.npz'
+        rad_coeff = 'data/prisma/PRS_Mali1_radcoeff_surface.npz'
+
+        landsat_url = run_config['inputs']['landsat']
+        landsat_tar = 'input/%s' % os.path.basename(landsat_url)
+
+        download_file(landsat_tar,
+                      landsat_url)
+
+        with tarfile.open(landsat_tar, 'r') as tar_ref:
+            tar_ref.extractall('input')
+
+        landsat = landsat_tar[:-7]
+
+        prisma.he5_to_envi('input/%s' % base_name,
+                           'output/',
+                           'temp/',
                            aws_cop_url,
-                           shift = args.smile,
-                           rad_coeff =args.rad_coeff,
+                           shift = smile,
+                           rad_coeff =rad_coeff,
                            proj = True,
-                           match=args.landsat)
+                           match=landsat)
 
-
-        l1p_dir = glob.glob("%s/PRS*" % args.out_dir)[0]
-        datetime =  "%sT%s" %  (base_name[16:24],base_name[24:30])
+        l1p_dir = glob.glob('output/PRS*')[0]
+        datetime =  '%sT%s' %  (base_name[16:24],base_name[24:30])
         sensor = 'PRISMA'
 
     elif base_name.startswith('ang') or base_name.startswith('f'):
-        aviris.preprocess(args.l1_file,args.out_dir,args.temp_dir,
-                          res = args.resolution)
-        sensor = 'AVIRIS'
+        aviris.preprocess('input/%s' % base_name,
+                           'output/',
+                           'temp/',
+                           res = 30)
+        l1p_dir = glob.glob('output/S*')[0]
+        sensor = os.path.basename(l1p_dir).split('_')[1]
 
     elif base_name.startswith('DESIS'):
-        desis.l1c_process(args.l1_file,args.out_dir,args.temp_dir,
+        desis.l1c_process('input/%s' % base_name,
+                           'output/',
+                           'temp/',
                            aws_cop_url)
 
-        l1p_dir = glob.glob("%s/DESIS*" % args.out_dir)[0]
+        l1p_dir = glob.glob('output/DESIS*')[0]
 
         # Get starting time of image acquisition
-        header_file = glob.glob(l1p_dir + "/*rdn_prj.hdr")[0]
+        header_file = glob.glob(l1p_dir + '/*rdn_prj.hdr')[0]
         header = parse_envi_header(header_file)
         datetime =header['start acquisition time'].replace('-','').replace(':','')[:-1]
         sensor = 'DESIS'
 
     else:
-        print('Unrecognized input sensor')
-
+        print("Unrecognized input sensor")
 
     #Rename DESIS and PRISMA output files
     if sensor in ['PRISMA','DESIS']:
-        for file in glob.glob("%s/*" % l1p_dir):
-            if 'loc' in file:
-                product = 'LOC'
+        for file in glob.glob('%s/*' % l1p_dir):
+            if ('loc' in file) & ('.csv' not in file):
+                product = '_LOC'
             elif 'obs' in file:
-                product = 'OBS'
+                product = '_OBS'
+            elif'rdn' in file:
+                product = ''
             else:
-                product = 'RDN'
+                continue
 
             ext = os.path.splitext(file)[-1]
+            if ext == '':
+                ext = '.bin'
+
             old_file = os.path.basename(file)
-            new_file = "SISTER_%s_%s_L1B_%s_000%s" % (sensor,datetime,product,ext)
-            os.rename("%s/%s" % (l1p_dir,old_file),
-                      "%s/%s" % (l1p_dir,new_file))
-        os.rename(l1p_dir,
-                  "%s/SISTER_%s_%s_L1B_RDN_000" %  (os.path.dirname(l1p_dir),sensor,datetime))
+            new_file = 'SISTER_%s_L1B_RDN_%s_CRID%s%s' % (sensor,datetime,product,ext)
+
+            os.rename('%s/%s' % (l1p_dir,old_file),
+                      'output/%s' % (new_file))
+
+        shutil.rmtree(l1p_dir)
+
+    rdn_file =  glob.glob("output/*CRID.bin")[0]
+
+    generate_quicklook(rdn_file,'output/')
+
+    for file in glob.glob("output/*.bin"):
+        generate_metadata(file.replace('.bin','.hdr'),
+                          'output/')
+
+    shutil.copyfile(run_config_json,
+                    'output/%s_runconfig.json' % os.path.basename(rdn_file)[:-4])
 
 
+def generate_quicklook(input_file,output_dir):
 
+    img = ht.HyTools()
+    img.read_file(input_file)
+    image_file ="%s/%s.png" % (output_dir,
+                               img.base_name)
 
+    if 'DESIS' in img.base_name:
+        band3 = img.get_wave(560)
+        band2 = img.get_wave(850)
+        band1 = img.get_wave(660)
+    else:
+        band3 = img.get_wave(560)
+        band2 = img.get_wave(850)
+        band1 = img.get_wave(1660)
+
+    rgb=  np.stack([band1,band2,band3])
+    rgb[rgb == img.no_data] = np.nan
+
+    rgb = np.moveaxis(rgb,0,-1).astype(float)
+    bottom = np.nanpercentile(rgb,5,axis = (0,1))
+    top = np.nanpercentile(rgb,95,axis = (0,1))
+    rgb = np.clip(rgb,bottom,top)
+    rgb = (rgb-np.nanmin(rgb,axis=(0,1)))/(np.nanmax(rgb,axis= (0,1))-np.nanmin(rgb,axis= (0,1)))
+    rgb = (rgb*255).astype(np.uint8)
+
+    im = Image.fromarray(rgb)
+    im.save(image_file)
+
+def generate_metadata(header_file,output_dir):
+
+    header = parse_envi_header(header_file)
+    base_name =os.path.basename(header_file)[:-4]
+
+    metadata = {}
+    metadata['sensor'] = header['sensor type'].upper()
+    metadata['start_time'] = header['start acquisition time'].upper()
+    metadata['end_time'] = header['end acquisition time'].upper()
+    metadata['description'] = header['description'].capitalize()
+
+    # Split corner coordinates string into list
+    coords = [float(x) for x in header['bounding box'].replace(']','').replace('[','').split(',')]
+
+    metadata['bounding_box'] = [list(x) for x in zip(coords[::2],coords[1::2])]
+    metadata['product'] = base_name.split('_')[4]
+    metadata['processing_level'] = base_name.split('_')[2]
+
+    config_file = '%s/%s.met.json' % (output_dir,base_name)
+
+    with open(config_file, 'w') as outfile:
+        json.dump(metadata,outfile,indent=3)
 
 if __name__=='__main__':
 
