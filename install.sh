c0a0219d07fb433fb8d9ffccc9c26d1fa1bb789d
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-02-07 09:59:58 -0800

    Updated dependencies

--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
-run_dir=$('pwd')
-imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
-pge_dir=$(dirname ${imgspec_dir})
-
 # Need to do custom install to prevent dependency errors
 conda create -y --name sister python=3.8
 source activate sister
 conda install -y gdal
 
-git clone -b sister-dev https://github.com/EnSpec/sister.git
+git clone -b 1.2.0 https://github.com/EnSpec/sister.git
 cd sister
 pip install .
 
-git clone https://github.com/EnSpec/hytools.git
+git clone -b 1.5.0 https://github.com/EnSpec/hytools.git
 cd hytools
 pip install .
-
